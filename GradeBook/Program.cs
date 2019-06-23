bcd48de7492afbbd5c6a692dc3dcd8c4401d740f
Andy Lewis <andybeans@hotmail.co.uk>
2019-06-23 20:13:10 +0100

    Array but no loop

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -6,7 +6,14 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if(args.Length > 0)
+            var numbers = new double[3];
+            numbers[0] = 12.7;
+            numbers[1] = 34.1;
+            numbers[2] = 10.3;
+            var result = numbers[0] + numbers[1] + numbers[2];
+            Console.WriteLine(result);
+
+            if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
             }
