9171e23db05fa5c9a0a7135c9b3e11239f1c6ef9
Andy Lewis <andybeans@hotmail.co.uk>
2020-05-09 20:32:28 +0100

    Chapter 3 finished

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,21 +7,17 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var numbers = new double[3];
-            numbers[0] = 12.7;
-            numbers[1] = 34.1;
-            numbers[2] = 10.3;
-            var result = numbers[0] + numbers[1] + numbers[2];
-            Console.WriteLine(result);
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
-            if (args.Length > 0)
+            var result = 0.0;
+            foreach (double number in grades)
             {
-                Console.WriteLine($"Hello, {args[0]}!");
+                result += number;
             }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
         }
     }
 }
