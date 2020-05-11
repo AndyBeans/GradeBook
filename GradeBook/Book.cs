f450ceb22e9a80b97773684f62c5cc3e47caed31
Andy Lewis <andybeans@hotmail.co.uk>
2020-05-11 20:40:32 +0100

    Ifs, loops and switches

--- GradeBook/Book.cs
+++ GradeBook/Book.cs
@@ -12,9 +12,38 @@ namespace GradeBook
             this.name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+            }
+        }
+
         public void AddGrade(double grade)
         {
-            grades.Add(grade);
+            if (grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
         }
 
         public Statistics GetStatistics()
@@ -24,14 +53,62 @@ namespace GradeBook
             result.Low = double.MaxValue;
             result.High = double.MinValue;
 
-            foreach (double grade in grades)
-            {
-                result.Low = Math.Min(grade, result.Low);
-                result.High = Math.Max(grade, result.High);
-                result.Average += grade;
-            }
+            //foreach (double grade in grades)
+            //{
+            //    result.Low = Math.Min(grade, result.Low);
+            //    result.High = Math.Max(grade, result.High);
+            //    result.Average += grade;
+            //}
 
+            //var index = 0;
+            //do
+            //{
+            //    result.Low = Math.Min(grades[index], result.Low);
+            //    result.High = Math.Max(grades[index], result.High);
+            //    result.Average += grades[index];
+            //    index++;
+            //} while (index < grades.Count);
+
+            //var index = 0;
+            //while (index < grades.Count)
+            //{
+            //    result.Low = Math.Min(grades[index], result.Low);
+            //    result.High = Math.Max(grades[index], result.High);
+            //    result.Average += grades[index];
+            //    index++;
+            //};
+
+            for (var index = 0; index < grades.Count; index++)
+            {
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+            }
+            
             result.Average /= grades.Count;
+
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
